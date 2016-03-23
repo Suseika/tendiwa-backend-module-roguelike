eaf99c8ba1c39893e5eeebf3de075e15d2ef1024
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-23 17:48:49 +0500

    Implement dropping items for player's inventory on the floor with D key. #T-2 Fixed

--- src/main/kotlin/org/tendiwa/backend/modules/roguelike/playerVolition/PlayerVolitionExtensions.kt
+++ src/main/kotlin/org/tendiwa/backend/modules/roguelike/playerVolition/PlayerVolitionExtensions.kt
@@ -2,9 +2,18 @@ package org.tendiwa.backend.modules.roguelike.playerVolition
 
 import org.tendiwa.backend.modules.roguelike.archetypes.Item
 import org.tendiwa.backend.modules.roguelike.aspects.inventory
+import org.tendiwa.backend.space.Voxel
 import org.tendiwa.frontend.generic.PlayerVolition
 
 fun PlayerVolition.pickUp(item: Item) {
     item.removeFromSpace(reality)
     this.host.inventory.store(reality, item)
+}
+
+/**
+ * Drops an item from player's [Inventory].
+ */
+fun PlayerVolition.drop(item: Item, voxel: Voxel) {
+    this.host.inventory.remove(reality, item)
+    item.addToSpace(reality, voxel)
 }
